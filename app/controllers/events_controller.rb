c41f9f1298bb993d63d18fe2bb6272768e1ab7a2
joe <joe.ulica@gmail.com>
2022-08-26 16:21:49 -0400

    add automatic signup for user when creating event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,6 +10,8 @@ class EventsController < ApplicationController
       p time
       event = Event.new(sport_id: params[:sport_id], location_id: params[:location_id], time: time, user_id: current_user.id)
       event.save
+      event_participant = EventParticipant.new(user_id: current_user.id, event_id: event.id)
+      event_participant.save
       render json: event
     else
       render json: {message: "you must be logged in to create an event"} #add error code here
